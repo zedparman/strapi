155c805f0176c05f8e3ef8ba34f0f6d513f4e8ad
zedparman <58415963+zedparman@users.noreply.github.com>
2021-08-13 12:30:11 +0430

    Create server.js

---
+++ myapp/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd53d0a48eee2802bb9ea048ce5d36874'),
+    },
+  },
+});
